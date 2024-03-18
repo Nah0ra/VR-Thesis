e66ce344d11063408b27d5dc65ef16a45e559836
Nah0ra <nah0ra@outlook.com>
2024-03-18 11:01:35 +0100

    - Added Spawning

--- Assets/Scripts/ItemSpawner.cs
+++ Assets/Scripts/ItemSpawner.cs
@@ -1,10 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemSpawner : MonoBehaviour
 {
+
+    [SerializeField]
+    private List<GameObject> SpawnedObjects;
+
+    [SerializeField]
+    private List<GameObject> SpawnAbles;
+
+    [SerializeField]
+    private GameObject ResetItems;
+
+    private GameObject Player;
+
+    private void Awake() 
+    {
+        ResetItems.GetComponent<Button>().onClick.AddListener(DeleteItems);
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
     public void Reveal()
     {
         gameObject.SetActive(true);
@@ -13,5 +33,25 @@ public class ItemSpawner : MonoBehaviour
     public void Hide()
     {
         gameObject.SetActive(false);
+    }
+
+    public void SpawnItem(string Item)
+    {
+        switch (Item)
+        {
+            case "Sofa":
+                GameObject Spawned = Instantiate(SpawnAbles[0], gameObject.transform.position, Quaternion.identity);
+                SpawnedObjects.Add(Spawned);
+                break;
+        }
+    }
+
+    public void DeleteItems()
+    {
+        foreach (GameObject item in SpawnedObjects)
+        {
+            Destroy(item);
+        }
+        SpawnedObjects.Clear();
     }
 }
