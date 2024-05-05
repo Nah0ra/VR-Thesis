0ebf5719c6cccb99602cfc3967469b54956b22f7
Nah0ra <nah0ra@outlook.com>
2024-05-05 11:36:16 +0200

    - Adjusted spawning panel

---
+++ Assets/Scripts/Room Spawner.cs
@@ -0,0 +1,32 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class RoomSpawner : MonoBehaviour
+{
+    private GameObject LengthSlider;
+    private GameObject WidthSlider;
+    private GameObject HeightSlider;
+
+    private TextMeshProUGUI LengthValue;
+    private TextMeshProUGUI WidthValue;
+    private TextMeshProUGUI HeightValue;
+
+    
+
+    private void Start() 
+    {
+        LengthSlider = GameObject.Find("LengthSlider").gameObject;
+        WidthSlider = GameObject.Find("WidthSlider").gameObject;
+        HeightSlider = GameObject.Find("HeightSlider").gameObject;
+
+        LengthValue = LengthSlider.transform.GetChild(1).transform.GetChild(3).transform.GetComponent<TextMeshProUGUI>();
+        WidthValue = WidthSlider.transform.GetChild(1).transform.GetChild(3).transform.GetComponent<TextMeshProUGUI>();
+        HeightValue = HeightSlider.transform.GetChild(1).transform.GetChild(3).transform.GetComponent<TextMeshProUGUI>();
+    }
+    
+    private void FixedUpdate() 
+    {
+        LengthValue.text = LengthSlider.transform.GetChild(1).GetComponent<Slider>().value.ToString();
+    }
+}
