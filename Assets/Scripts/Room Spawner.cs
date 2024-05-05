9b07b33e2a0150dab226685f662ada4c40c4cc35
Nah0ra <nah0ra@outlook.com>
2024-05-05 13:26:23 +0200

    - Updated Room Spawner

--- Assets/Scripts/Room Spawner.cs
+++ Assets/Scripts/Room Spawner.cs
@@ -8,6 +8,9 @@ public class RoomSpawner : MonoBehaviour
     private GameObject LengthSlider;
     private GameObject WidthSlider;
     private GameObject HeightSlider;
+    private GameObject SpawnLocation;
+
+    public GameObject SpawnroomPrefab;
 
     private TextMeshProUGUI LengthValue;
     private TextMeshProUGUI WidthValue;
@@ -20,10 +23,11 @@ public class RoomSpawner : MonoBehaviour
         LengthSlider = GameObject.Find("LengthSlider").gameObject;
         WidthSlider = GameObject.Find("WidthSlider").gameObject;
         HeightSlider = GameObject.Find("HeightSlider").gameObject;
+        SpawnLocation = GameObject.Find("Spawn Location").gameObject;
 
-        LengthValue = LengthSlider.transform.GetChild(1).transform.GetChild(3).transform.GetComponent<TextMeshProUGUI>();
-        WidthValue = WidthSlider.transform.GetChild(1).transform.GetChild(3).transform.GetComponent<TextMeshProUGUI>();
-        HeightValue = HeightSlider.transform.GetChild(1).transform.GetChild(3).transform.GetComponent<TextMeshProUGUI>();
+        LengthValue = LengthSlider.transform.GetChild(1).transform.GetChild(2).transform.GetComponent<TextMeshProUGUI>();
+        WidthValue = WidthSlider.transform.GetChild(1).transform.GetChild(2).transform.GetComponent<TextMeshProUGUI>();
+        HeightValue = HeightSlider.transform.GetChild(1).transform.GetChild(2).transform.GetComponent<TextMeshProUGUI>();
     }
     
     private void FixedUpdate() 
@@ -31,5 +35,21 @@ public class RoomSpawner : MonoBehaviour
         LengthValue.text = LengthSlider.transform.GetChild(1).GetComponent<UnityEngine.UI.Slider>().value.ToString();
         WidthValue.text = WidthSlider.transform.GetChild(1).GetComponent<UnityEngine.UI.Slider>().value.ToString();
         HeightValue.text = HeightSlider.transform.GetChild(1).GetComponent<UnityEngine.UI.Slider>().value.ToString();
+    }
+
+    public void SpawnRoom()
+    {
+        if (GameObject.FindWithTag("Room") != null)
+        {
+            Destroy(GameObject.FindWithTag("Room").gameObject);
+            GameObject SpawnedRoom = Instantiate(SpawnroomPrefab,SpawnLocation.transform,false);
+            SpawnedRoom.transform.localScale = new Vector3(int.Parse(LengthValue.text),int.Parse(WidthValue.text),int.Parse(HeightValue.text));
+        }
+        else
+        {
+            GameObject SpawnedRoom = Instantiate(SpawnroomPrefab,SpawnLocation.transform,false);
+            SpawnedRoom.transform.localScale = new Vector3(int.Parse(LengthValue.text),int.Parse(WidthValue.text),int.Parse(HeightValue.text));
+        }
+        
     }
 }
