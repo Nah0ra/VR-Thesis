85500a050d7abf6e659748d877303f6493785472
Nah0ra <nah0ra@outlook.com>
2024-05-14 21:19:43 +0200

    - Added Chair 3

--- Assets/Scripts/Item Spawner.cs
+++ Assets/Scripts/Item Spawner.cs
@@ -29,6 +29,11 @@ public class ItemSpawner : MonoBehaviour
                 GameObject Chair2 = Instantiate(SpawnAbles[2], SpawnArea.transform.position, Quaternion.Euler(-90,0,0));
                 SpawnedItems.Add(Chair2);
                 break;
+
+            case "Chair3":
+                GameObject Chair3 = Instantiate(SpawnAbles[3], SpawnArea.transform.position, Quaternion.Euler(-90,0,0));
+                SpawnedItems.Add(Chair3);
+                break;
         }
     }
 
